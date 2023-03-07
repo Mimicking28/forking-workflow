fce1edd42b3f0620a783f19487c6af3322c4e324
Zachary Legaria <gmzach024@gmail.com>
2023-03-07 11:44:48 +0800

    forking

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,11 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	printf("\n\nAlbert Mick Estillore")
+	printf("\n\nAlbert Mick Estillore");
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
-}
+} gi
